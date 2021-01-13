c2ae9950344f1b8676b6a9d3ad58f6facecb762e
Mykhailo1257Kholmohorov <mykhailo1257@gmail.com>
2021-01-13 14:10:01 +0200

    test SSH connection

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace gitPractiseFromUdemyCourse
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
