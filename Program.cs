ef01eebc41a07bdccac5f17243f6040a5921c32f
Mykhailo1257Kholmohorov <mykhailo1257@gmail.com>
2021-01-19 22:54:14 +0200

    update

--- Program.cs
+++ Program.cs
@@ -4,9 +4,6 @@ namespace gitPractiseFromUdemyCourse {
     class Program {
         static void Main (string[] args) {
             Console.WriteLine ("Hello World!");
-
-
-            
             Console.WriteLine ("Hello World!");
         }
     }
